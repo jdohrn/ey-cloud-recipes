45b379bb95ab827536660b24cf95ea3f12c12120
jdohrn <joedohrn@Joes-MacBook-Pro.local>
2016-07-25 18:04:31 -0500

    20160725

---
+++ cookbooks/daemons/recipes/default.rb
@@ -0,0 +1,46 @@
+
+daemons_to_load = [
+  { :command_name => 'carrier_tenderer', 
+    :command_to_run => 'bundle exec rake daemons:carrier_tenderer RAILS_ENV=production',
+    :app_name => 'synergy',
+    :run_in_background => true
+  },
+  { :command_name => 'pickup_monitor', 
+    :command_to_run => 'bundle exec rake pickup_monitor:testies RAILS_ENV=production',
+    :app_name => 'synergy',
+    :run_in_background => true
+  }
+]
+
+if ['util'].include?(node[:instance_role])
+  service "monit" do
+    supports :reload => true
+    action :enable
+  end
+  
+  daemons_to_load.each do |d|
+    
+    template "/usr/local/bin/#{command_name}_wrapper" do
+      owner node[:owner_name]
+      group node[:group_name]
+      mode 0755
+      source "wrapper.sh.erb"
+      variables({:command_name => d[:command_name],
+                 :command_to_run => command_to_run,
+                 :run_in_background => run_in_background})
+    end
+
+    template "/etc/monit.d/#{app_name}_#{command_name}.monitrc" do
+      owner 'root'
+      group 'root'
+      mode 0644
+      source 'monitrc.erb'
+      variables({:app_name => app_name,
+                 :command_name => command_name,
+                 :command_to_run => command_to_run})
+      notifies :reload, resources(:service => 'monit')
+    end
+    
+  end
+
+end
